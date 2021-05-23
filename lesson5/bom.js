1ef9cdcbbe77dc9a5be6b8a1c01cae97b8740de0
rawinolivera <oli18001@byui.edu>
2021-05-22 21:16:03 -0500

    updating

---
+++ lesson5/bom.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#favchap');
+
+const button = document.querySelector('#submit');
+
+const list = document.querySelector('.list');
+
+button.addEventListener('click', () => {
+
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('deletebutton');
+
+});
